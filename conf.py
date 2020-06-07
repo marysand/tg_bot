d7ec2429574557e54e8bf1f6dcf51fcf805aecb2
Maria <andreeva.maria.s@gmail.com>
2020-06-07 22:10:29 +0300

    second commit

---
+++ conf.py
@@ -0,0 +1,7 @@
+TOKEN = "1116521412:AAFMU3n97_kO3RlIgyMWgxgdj79IqLHaJto"
+
+PROXY = {
+    'https': 'Socks5://210478695:sQuculni@ss-01.s5.ynvv.cc:999',
+    'Socks5': 'Socks5://210478695:sQuculni@ss-01.s5.ynvv.cc:999'
+}
+
